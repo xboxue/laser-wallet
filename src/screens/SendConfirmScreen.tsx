67e2cff95234c561ed658cfbe48a69ae965de11d
Bowen Xue <xboxue@gmail.com>
2022-06-08 12:09:25 -0500

    Support sending ERC20 tokens

--- src/screens/SendConfirmScreen.tsx
+++ src/screens/SendConfirmScreen.tsx
@@ -1,15 +1,17 @@
 import { useNavigation } from "@react-navigation/native";
 import { ContractTransaction, ethers } from "ethers";
-import { formatUnits, parseEther } from "ethers/lib/utils";
+import { formatUnits, parseEther, parseUnits } from "ethers/lib/utils";
 import * as SecureStore from "expo-secure-store";
 import { Laser } from "laser-sdk/src";
 import { ENTRY_POINT_GOERLI } from "laser-sdk/src/constants";
 import { Box, Button, Skeleton, Stack, Text } from "native-base";
 import { useState } from "react";
-import { useBalance, useFeeData, useProvider } from "wagmi";
+import { erc20ABI, useBalance, useFeeData, useProvider } from "wagmi";
 import { entryPointAbi } from "../abis/TestEntryPoint.json";
 import useSecureStore from "../hooks/useSecureStore";
 import formatAddress from "../utils/formatAddress";
+
+const WETH_ADDRESS = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6";
 
 const SendConfirmScreen = ({ route }) => {
   const provider = useProvider({ chainId: 5 });
@@ -22,13 +24,65 @@ const SendConfirmScreen = ({ route }) => {
 
   const { data: feeData, isError, isLoading: loadingFeeData } = useFeeData();
 
-  const send = async () => {
+  const transferTokens = async () => {
     try {
       setSending(true);
       const walletAddress = await SecureStore.getItemAsync("walletAddress");
       const ownerPrivateKey = await SecureStore.getItemAsync("ownerPrivateKey");
       const { amount, address: to } = route.params;
-      if (!walletAddress || !ownerPrivateKey) throw new Error("what");
+      if (!walletAddress || !ownerPrivateKey) throw new Error();
+
+      const providerUrl =
+        "https://eth-goerli.alchemyapi.io/v2/e_-Jn9f06JUc7TXmtPdwzkI2TNdvjri1";
+
+      const owner = new ethers.Wallet(ownerPrivateKey);
+
+      const laser = new Laser(providerUrl, owner, walletAddress);
+
+      const erc20 = new ethers.Contract(WETH_ADDRESS, erc20ABI, provider);
+      const gas = await erc20
+        .connect(walletAddress)
+        .estimateGas.transfer(to, parseUnits(amount));
+      const feeData = await provider.getFeeData();
+
+      const userOp = await laser.transferERC20(
+        WETH_ADDRESS,
+        to,
+        parseUnits(amount),
+        {
+          callGas: gas.add(10000),
+          maxFeePerGas: feeData.maxFeePerGas,
+          maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
+        }
+      );
+
+      const entryPoint = new ethers.Contract(
+        ENTRY_POINT_GOERLI,
+        entryPointAbi,
+        owner.connect(provider)
+      );
+
+      const transaction: ContractTransaction = await entryPoint.handleOps(
+        [userOp],
+        owner.address,
+        { gasLimit: 250000 }
+      );
+      await transaction.wait();
+      await refetchBalance();
+      navigation.navigate("Home");
+    } catch (error) {
+      console.log(error);
+    }
+    setSending(false);
+  };
+
+  const sendEth = async () => {
+    try {
+      setSending(true);
+      const walletAddress = await SecureStore.getItemAsync("walletAddress");
+      const ownerPrivateKey = await SecureStore.getItemAsync("ownerPrivateKey");
+      const { amount, address: to } = route.params;
+      if (!walletAddress || !ownerPrivateKey) throw new Error();
 
       const providerUrl =
         "https://eth-goerli.alchemyapi.io/v2/e_-Jn9f06JUc7TXmtPdwzkI2TNdvjri1";
@@ -86,19 +140,25 @@ const SendConfirmScreen = ({ route }) => {
           </Box>
           <Box flexDirection="row" justifyContent="space-between">
             <Text variant="subtitle2">Gas (estimate)</Text>
-            <Text variant="subtitle2">
-              {!loadingFeeData && feeData ? (
-                formatUnits(
+            {!loadingFeeData && feeData ? (
+              <Text variant="subtitle2">
+                {formatUnits(
                   feeData.maxFeePerGas
                     .add(feeData.maxPriorityFeePerGas)
                     .mul(21000)
-                )
-              ) : (
-                <Skeleton />
-              )}
-            </Text>
+                )}{" "}
+                ETH
+              </Text>
+            ) : (
+              <Skeleton />
+            )}
           </Box>
-          <Button onPress={send} isLoading={sending}>
+          <Button
+            onPress={
+              route.params.token.symbol === "ETH" ? sendEth : transferTokens
+            }
+            isLoading={sending}
+          >
             Confirm
           </Button>
         </Stack>
