74b9ca8ccf1ed2b78be5a87b7817c5e644dd9f2f
Bowen Xue <xboxue@gmail.com>
2022-06-07 22:54:51 -0500

    Show all tokens in send flow

--- src/screens/SendAssetScreen.tsx
+++ src/screens/SendAssetScreen.tsx
@@ -1,15 +1,13 @@
 import { useNavigation } from "@react-navigation/native";
 import icon from "crypto-icons-plus-128/src/ethereum.png";
 import { Box, Image, Pressable, Text } from "native-base";
+import TokenBalances from "../components/TokenBalances/TokenBalances";
+import useSecureStore from "../hooks/useSecureStore";
 import useWalletContract from "../hooks/useWalletContract";
 
 const SendAssetScreen = ({ route }) => {
   const navigation = useNavigation();
-  const {
-    data: balance,
-    loading,
-    error,
-  } = useWalletContract("getBalanceInEth");
+  const walletAddress = useSecureStore("walletAddress");
 
   return (
     <Box>
@@ -17,31 +15,17 @@ const SendAssetScreen = ({ route }) => {
         <Text variant="subtitle1" mb="3">
           Choose asset
         </Text>
-        <Pressable
-          onPress={() =>
-            navigation.navigate("SendAmount", {
-              ...route.params,
-              asset: "eth",
-            })
-          }
-        >
-          {({ isPressed }) => (
-            <Box
-              flexDirection="row"
-              alignItems="center"
-              mt="2"
-              opacity={isPressed ? 0.2 : 1}
-            >
-              <Image source={icon} size="10" alt="ethereum-icon" />
-              <Text variant="subtitle1" ml="3">
-                ETH
-              </Text>
-              <Text variant="subtitle1" ml="auto">
-                {balance}
-              </Text>
-            </Box>
-          )}
-        </Pressable>
+        {walletAddress && (
+          <TokenBalances
+            walletAddress={walletAddress}
+            onPress={(token) =>
+              navigation.navigate("SendAmount", {
+                ...route.params,
+                token,
+              })
+            }
+          />
+        )}
       </Box>
     </Box>
   );
