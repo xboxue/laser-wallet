1726d86a1adb7559f448cd29364314d88c44ed07
Bowen Xue <xboxue@gmail.com>
2022-06-09 02:34:46 -0500

    Fix reset store on reinstall

--- src/store.ts
+++ src/store.ts
@@ -20,7 +20,7 @@ export const store = configureStore({
     }),
 });
 
-export const persistor = persistStore(store);
+export const getPersistor = () => persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
