004427c49afd42ed4491107cca28ef6d84c61f9b
Bowen Xue <xboxue@gmail.com>
2022-06-28 00:39:23 -0400

    Rename app to Laser and rebuild for android

--- android/app/src/main/java/com/laser/wallet/newarchitecture/MainApplicationReactNativeHost.java
+++ android/app/src/main/java/com/laser/wallet/newarchitecture/MainApplicationReactNativeHost.java
@@ -1,4 +1,4 @@
-package com.xboxue.walletapp.newarchitecture;
+package com.laser.wallet.newarchitecture;
 
 import android.app.Application;
 import androidx.annotation.NonNull;
@@ -19,9 +19,9 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.EmptyReactNativeConfig;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.xboxue.walletapp.BuildConfig;
-import com.xboxue.walletapp.newarchitecture.components.MainComponentsRegistry;
-import com.xboxue.walletapp.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
+import com.laser.wallet.BuildConfig;
+import com.laser.wallet.newarchitecture.components.MainComponentsRegistry;
+import com.laser.wallet.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
