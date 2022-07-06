9c6474268ea658b4afd0ffbc75953efbd21a06e9
Bowen Xue <xboxue@gmail.com>
2022-07-06 02:40:02 -0400

    Fix eth in token list on mainnet

--- src/hooks/useTokenBalances.ts
+++ src/hooks/useTokenBalances.ts
@@ -5,7 +5,7 @@ import abi from "../abis/BalanceChecker.abi.json";
 import { selectChainId } from "../features/network/networkSlice";
 
 const BALANCE_CONTRACT = {
-  mainnet: "0xb1f8e55c7f64d203c1400b9d8555d050f94adf39",
+  homestead: "0xb1f8e55c7f64d203c1400b9d8555d050f94adf39",
   ropsten: "0x8D9708f3F514206486D7E988533f770a16d074a7",
   rinkeby: "0x3183B673f4816C94BeF53958BaF93C671B7F8Cf2",
   kovan: "0x55ABBa8d669D60A10c104CC493ec5ef389EC92bb",
