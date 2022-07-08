2ad63ff9c8f72f493c83573021f2ffd3e4cadd98
Bowen Xue <xboxue@gmail.com>
2022-07-08 16:53:04 -0400

    Persist current chain

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,20 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { configureStore } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import createSecureStore from "redux-persist-expo-securestore";
 import authReducer from "./features/auth/authSlice";
-import networkReducer from "./features/network/networkSlice";
 import guardiansReducer from "./features/guardians/guardiansSlice";
+import networkReducer from "./features/network/networkSlice";
 import walletConnectReducer from "./features/walletConnect/walletConnectSlice";
 
 export const store = configureStore({
   reducer: {
     walletConnect: walletConnectReducer,
     guardians: guardiansReducer,
-    network: networkReducer,
+    network: persistReducer(
+      { key: "network", storage: AsyncStorage },
+      networkReducer
+    ),
     auth: persistReducer(
       { key: "auth", storage: createSecureStore() },
       authReducer
