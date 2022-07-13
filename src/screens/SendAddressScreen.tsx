32d9204fedc03808adbfa7b4010e080086e8773b
Bowen Xue <xboxue@gmail.com>
2022-07-13 16:16:39 -0400

    Support ENS names in send flow

--- src/screens/SendAddressScreen.tsx
+++ src/screens/SendAddressScreen.tsx
@@ -1,22 +1,76 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { useNavigation } from "@react-navigation/native";
+import { isAddress } from "ethers/lib/utils";
 import {
   Avatar,
   Box,
-  Button,
   Icon,
   Input,
   Pressable,
-  Stack,
+  Skeleton,
   Text,
 } from "native-base";
-import Ionicons from "@expo/vector-icons/Ionicons";
 import { useState } from "react";
-import { isAddress } from "ethers/lib/utils";
+import { useSelector } from "react-redux";
+import { useEnsAddress, useEnsAvatar } from "wagmi";
+import { selectChainId } from "../features/network/networkSlice";
 import formatAddress from "../utils/formatAddress";
 
 const SendAddressScreen = () => {
+  const chainId = useSelector(selectChainId);
   const navigation = useNavigation();
   const [value, setValue] = useState("");
+
+  const { data: ensAddress, isLoading: ensAddressLoading } = useEnsAddress({
+    name: value,
+    enabled: value.includes("."),
+    chainId,
+  });
+
+  const { data: ensAvatar, isLoading: ensAvatarLoading } = useEnsAvatar({
+    addressOrName: value,
+    enabled: !!ensAddress,
+    chainId,
+  });
+
+  const renderItem = () => {
+    if (isAddress(value))
+      return (
+        <Pressable
+          onPress={() => navigation.navigate("SendAsset", { address: value })}
+        >
+          <Box flexDirection="row" alignItems="center" mt="3">
+            <Avatar>0x</Avatar>
+            <Text ml="2" variant="subtitle1">
+              {formatAddress(value)}
+            </Text>
+          </Box>
+        </Pressable>
+      );
+
+    if (ensAddressLoading || ensAvatarLoading) return <Skeleton />;
+
+    if (ensAddress)
+      return (
+        <Pressable
+          onPress={() =>
+            navigation.navigate("SendAsset", { address: ensAddress })
+          }
+        >
+          <Box flexDirection="row" alignItems="center" mt="3">
+            <Avatar source={ensAvatar ? { uri: ensAvatar } : undefined}>
+              {value[0]}
+            </Avatar>
+            <Box ml="2">
+              <Text variant="subtitle1">{value}</Text>
+              <Text>{formatAddress(ensAddress)}</Text>
+            </Box>
+          </Box>
+        </Pressable>
+      );
+
+    if (value) return <Text mt="3">Invalid address</Text>;
+  };
 
   return (
     <Box>
@@ -36,21 +90,11 @@ const SendAddressScreen = () => {
           InputLeftElement={
             <Icon as={Ionicons} name="search-outline" ml="3" size="5" />
           }
+          autoCorrect={false}
+          autoCapitalize="none"
+          mb="1"
         />
-        {isAddress(value) ? (
-          <Pressable
-            onPress={() => navigation.navigate("SendAsset", { address: value })}
-          >
-            <Box flexDirection="row" alignItems="center" mt="3">
-              <Avatar>0x</Avatar>
-              <Text ml="2" variant="subtitle1">
-                {formatAddress(value)}
-              </Text>
-            </Box>
-          </Pressable>
-        ) : (
-          value && <Text mt="3">Invalid address</Text>
-        )}
+        {renderItem()}
       </Box>
     </Box>
   );
