1ffeb44094cfd1ed432f2c4ddbcd1f0b55e8a01b
Bowen Xue <xboxue@gmail.com>
2022-08-03 16:40:46 -0400

    Fix react-query logging

--- App.tsx
+++ App.tsx
@@ -16,7 +16,12 @@ import * as SecureStore from "expo-secure-store";
 import { NativeBaseProvider } from "native-base";
 import { useEffect } from "react";
 import { MMKV } from "react-native-mmkv";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import * as Sentry from "sentry-expo";
@@ -74,11 +79,16 @@ Sentry.init({
 });
 
 const queryClient = new QueryClient({
-  logger: {
-    log: Sentry.Native.captureMessage,
-    warn: Sentry.Native.captureMessage,
-    error: Sentry.Native.captureException,
-  },
+  queryCache: new QueryCache({
+    onError: (error) => {
+      Sentry.Native.captureException(error);
+    },
+  }),
+  mutationCache: new MutationCache({
+    onError: (error) => {
+      Sentry.Native.captureException(error);
+    },
+  }),
 });
 
 const App = () => {
