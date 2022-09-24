13a99d063f37a43f216e01824bdc072825a82c89
Bowen Xue <xboxue@gmail.com>
2022-09-24 03:31:28 -0400

    Bump runtime version

--- app.config.js
+++ app.config.js
@@ -17,7 +17,7 @@ export default {
     url: "https://u.expo.dev/7a661128-fd6d-4dce-b72e-2f5f2ead8a58",
   },
   assetBundlePatterns: ["**/*"],
-  runtimeVersion: "1.0.0",
+  runtimeVersion: "1.0.4",
   ios: {
     supportsTablet: false,
     buildNumber: "1",
