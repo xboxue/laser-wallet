d64ed799b2a3d4fbe26ea485433bac097e1d50c1
Bowen Xue <xboxue@gmail.com>
2022-09-29 14:33:16 -0400

    Improve keychain error handling

--- src/services/keychain.ts
+++ src/services/keychain.ts
@@ -4,22 +4,63 @@ import {
   getInternetCredentials,
   AuthenticationPrompt,
 } from "react-native-keychain";
+import * as Sentry from "sentry-expo";
 
-export const setItem = (key: string, value: string, options?: Options) => {
-  return setInternetCredentials(key, key, value, options);
+export const setItem = async (
+  key: string,
+  value: string,
+  options?: Options
+) => {
+  try {
+    await setInternetCredentials(key, key, value, options);
+  } catch (error) {
+    Sentry.Native.captureMessage("Keychain write first attempt failed");
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    try {
+      await setInternetCredentials(key, key, value, options);
+    } catch (error) {
+      Sentry.Native.captureMessage("Keychain write second attempt failed");
+      Sentry.Native.captureException(error);
+      throw error;
+    }
+  }
 };
 
 export const getItem = async (
   key: string,
   authenticationPrompt?: AuthenticationPrompt
 ) => {
-  const credentials = await getInternetCredentials(key, {
+  const options = {
     authenticationPrompt: {
       title: "Laser",
       subtitle: "Unlock your wallet using your fingerprint",
       ...authenticationPrompt,
     },
-  });
-  if (!credentials) return null;
-  return credentials.password;
+  };
+
+  try {
+    const credentials = await getInternetCredentials(key, options);
+    if (!credentials) return null;
+    return credentials.password;
+  } catch (error) {
+    if (
+      error.message ===
+      "The user name or passphrase you entered is not correct."
+    ) {
+      Sentry.Native.captureMessage("Keychain read first attempt failed");
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      try {
+        const credentials = await getInternetCredentials(key, options);
+        if (!credentials) return null;
+        return credentials.password;
+      } catch (error) {
+        Sentry.Native.captureMessage("Keychain read second attempt failed");
+        Sentry.Native.captureException(error);
+        throw error;
+      }
+    }
+    throw error;
+  }
 };
